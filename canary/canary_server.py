59a4d5803f6193a336945ce94fdba4f4cbe74ea9
Jake Fenton <jake@fenton.io>
2018-08-22 19:05:18 -0400

    and the flag has to be set in the right place

--- canary/canary_server.py
+++ canary/canary_server.py
@@ -87,9 +87,9 @@ class Notifier(threading.Thread):
             timeout_point = datetime.datetime.now() - datetime.timedelta(seconds=TIMEOUT_IN_SECONDS)
             if shared_timestamp is None:
                 print("Something odd happened.... (Have we made a connection yet?)")
-                self.canary_alive = True
             elif not self.canary_alive:
                 print("Fridge seen for the first time!")
+                self.canary_alive = True
             elif self.canary_alive and shared_timestamp < timeout_point:
                 print("The canary died!")
                 Notifier.notify("The canary has died.", "Fridge Down")
